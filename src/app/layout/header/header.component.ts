ffe6d3dd0744e5c94b725d1f412ebb15034859ec
j4dev <jesusguanga98@outlook.com>
2020-12-03 15:51:29 -0500

    Cerrar Sesión

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AuthService } from "../../shared/services/auth.service";
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  user_name:string = "";
+  constructor(
+    private authservice:AuthService
+  ) { }
 
   ngOnInit(): void {
+    this.user_name = localStorage.getItem('user_name') + "  ";
+    
   }
 
+  logoutUser(){
+    this.authservice.logout();
+  }
 }
--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -1,13 +1,25 @@
 import { Injectable } from '@angular/core';
 
+import { Router } from "@angular/router";
+
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
   
   getToken() {
     return localStorage.getItem('email')
-	}
+  }
+  
+  logout() {
+    // remove user from local storage
+    localStorage.removeItem('user_name');
+    localStorage.removeItem('email');
+    localStorage.removeItem('user_tipo');
+    this.router.navigate(['/login']);
+  }
 }
