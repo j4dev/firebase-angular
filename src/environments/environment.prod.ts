632fa97ae028ef3fad4123a64bbc9d72027e6833
j4dev <jesusguanga98@outlook.com>
2020-12-03 01:03:52 -0500

    Configuración de firebase.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyB6OHFZ4ZaW6Dub3HViHcsdxQNPAegm1mE",
+    authDomain: "hotel-hosop.firebaseapp.com",
+    projectId: "hotel-hosop",
+    storageBucket: "hotel-hosop.appspot.com",
+    messagingSenderId: "493065100495",
+    appId: "1:493065100495:web:11d8451e565c673c442bba",
+    measurementId: "G-2NKCQW6M62"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyB6OHFZ4ZaW6Dub3HViHcsdxQNPAegm1mE",
+    authDomain: "hotel-hosop.firebaseapp.com",
+    projectId: "hotel-hosop",
+    storageBucket: "hotel-hosop.appspot.com",
+    messagingSenderId: "493065100495",
+    appId: "1:493065100495:web:11d8451e565c673c442bba",
+    measurementId: "G-2NKCQW6M62"
+  }
 };
 
 /*
