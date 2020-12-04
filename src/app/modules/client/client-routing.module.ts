3199abf45384b0f4974c3da4f7b3dc741d635e89
j4dev <jesusguanga98@outlook.com>
2020-12-04 07:46:13 -0500

    Seleccion de metodo de pago.

--- src/app/modules/client/client-routing.module.ts
+++ src/app/modules/client/client-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { PlansComponent } from "./components/plans/plans.component";
+import { PaymentComponent } from "./components/payment/payment.component";
 
 const routes: Routes = [
   {
     path: '',
     component: PlansComponent
+  },
+  {
+    path: 'pago',
+    component: PaymentComponent
   },
 ];
 
